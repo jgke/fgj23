b3cb066e27b231552329d9fd0b43548131329326
Jani Suutarinen <jani.suutarinen@gmail.com>
2023-02-04 21:58:08 +0200

    Add start and end exposition for levels 0-5 (1-8)

--- FGJ23/Levels/LevelBank.cs
+++ FGJ23/Levels/LevelBank.cs
@@ -166,14 +166,49 @@ namespace FGJ23.Levels
             {
                 "level1" =>
                     new StoryBuilder()
-                    .Exposition("Only expo here")
-                    .Exposition("No characters")
+                    .Exposition("I was born in 1982. Back in my day, games were about CONTENT. You'd walk around with the arrow keys. Jump by pressing up.")
+                    .Exposition("Maybe shoot some bad guys with the space bar. Double jump by jumping again in the air and wall jump by pointing towards a wall in mid-air and jumping. The good old days")
+                    .Exposition("Nowadays the kids can’t stay focused for more than two seconds unless it’s some virtual reality thirst trap.")
                     .GoToLevel(),
 
                 "level2" =>
                     new StoryBuilder()
-                    .Exposition("lev2 start story")
-                    .Exposition("blah")
+                    .Exposition("2082. What a year this could've been for games. A century of evolution from the legends of yore.")
+                    .Exposition("Every first level should be easy, but showcase the main features without hand-holding.")
+                    .Exposition("There's an enemy. You can shoot it. You may need to jump over things to progress.")
+                    .GoToLevel(),
+
+                "level3" =>
+                    new StoryBuilder()
+                    .Exposition("Sometimes just jumping is not enough. Sometimes an enemy is in your way. Sometimes, you have to make the right choices")
+                    .Exposition("And a grand finale, a true puzzle! Do I jump here or there?")
+                    .GoToLevel(),
+
+                "level4" =>
+                    new StoryBuilder()
+                    .Exposition("That didn't feel right. It has to be rewarding! You have to use all your jumps to clear it!")
+                    .GoToLevel(),
+
+                "level5" =>
+                    new StoryBuilder()
+                    .Exposition("THERE we go! A proper, pure challenge! Hit it just right. Special jumps are for whippersnappers.")
+                    .GoToLevel(),
+
+                "level6" =>
+                    new StoryBuilder()
+                    .Exposition("Graphics became frivolous much too early. You don’'t need more than a suggestion. A hint to jump-start your own imagination. No spoon-fed high-definition dreamscapes")
+                    .Exposition("Change of pace. Enemies in the way. More of them. A challenge of a different kind. A satisfying contrast.")
+                    .GoToLevel(),
+
+                "level7" =>
+                    new StoryBuilder()
+                    .Exposition("Shooting from afar is cowardly. You have to make it personal. Look losing your only life right in the eye to clear every single monster. ")
+                    .GoToLevel(),
+
+                "level8" =>
+                    new StoryBuilder()
+                    .Exposition("Back in my day, every mechanic was necessary. No useless frills.\r\n")
+                    .Exposition("You jump higher if you jump on an enemy. That's a clue. You need it somewhere. Use it.")
                     .GoToLevel(),
 
                 _ => throw new Exception("Unknown level name: " + name),
@@ -187,15 +222,46 @@ namespace FGJ23.Levels
             {
                 "level1" =>
                     new StoryBuilder()
-                    .Exposition("lev1 end story")
-                    .Exposition("blah")
+                    .Exposition("Ever since the 2050's, kids have been unable to function without constantly tending to their retinal and cochlear implants.")
+                    .Exposition("Sharing four-dimensional memes. Living third lives on some D-Corporation Hyperverse.")
+                    .Exposition("I want none of that. I just want a challenge and to reach that castle on the far right.")
+                    .Exposition("I retired this year at the ripe old age of one hundred, and I intend to spend the rest of my days enjoying what games should always have been about!")
                     .GoToLevel(),
 
                 "level2" =>
                     new StoryBuilder()
-                    .Exposition("lev2 end story")
-                    .Exposition("blah")
+                    .Exposition("Simple. Pure. Beautiful.")
                     .GoToLevel(),
+
+                "level3" =>
+                    new StoryBuilder()
+                    .GoToLevel(),
+
+                "level4" =>
+                    new StoryBuilder()
+                    .GoToLevel(),
+
+                "level5" =>
+                    new StoryBuilder()
+                    .GoToLevel(),
+
+                "level6" =>
+                    new StoryBuilder()
+                    .Exposition("You really gave it to them!")
+                    .GoToLevel(),
+                    
+                "level7" =>
+                    new StoryBuilder()
+                    .Exposition("THIS is true skill. THIS is how you feel accomplishment. ")
+                    .GoToLevel(),
+
+                "level8" =>
+                     new StoryBuilder()
+                    .Exposition("Chekhov’s guns are sloppy writing.")
+                    .Exposition("This is perfect form, that requires perfect execution of the player. Exquisite.")
+                    .GoToLevel(),
+
+
 
                 _ => throw new Exception("Unknown level name: " + name),
             };
